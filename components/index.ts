8d995785cba909b72eeac935c57b0adac5ffc970
Sk.Almamun Ali <94174675+almamunali@users.noreply.github.com>
2023-07-17 13:25:24 +0530

    adding import and export one file to ...

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,7 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 
-
-export {Hero,CustomButton};
+export {Hero,CustomButton,NavBar,Footer}; 
